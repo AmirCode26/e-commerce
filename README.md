c68ac89eac8a6b148aa3a54244ed659619856821
Amircode26 <austin26123@gmail.com>
2026-07-24 19:08:20 -0400

    some "reponsive" added

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
-
+# That's my first "Paid" services as a web Developer :)
 ## Getting Started
 
-First, run the development server:
+First, run the development server (Obviusly):
 
 ```bash
+bun run dev <---- For performanence
+# or
 npm run dev
 # or
 yarn dev
@@ -15,22 +16,16 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Also u can  see the app on the following domain: (Oh really, thats project don't  have a domain yet)
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+You can start editing the page by modifying `app/page.tsx`. Sorry if it have bugs haha :)
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+## How it works? 
+**IDK, but it works 0.0**
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+Just kidding, first, when u enter to the website, u can see
+a menu (That's  in Spanish cause me and my client speak Spanish)
+There  is a  picture: (PC)
+![Home Screenshot](./src/archive/etc/home_screenshot.png)
+(Mobile)
+![Home Screenshot](./src/archive/etc/home_screenshot_mobile.png)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Astroid, Baby, Globe, Shirt } from "lucide-react";
+import { Astroid } from "lucide-react";
 import Image from "next/image";
 import { Lobster_Two } from "next/font/google";
 import { cn } from "@/lib/utils";
@@ -10,10 +10,10 @@ export default function Home() {
   const { pages_anchors } = useHomeContext().navigateContext;
 
   return (
-    <div className="z-100 fixed left-0 w-screen bg-[#faf0e7dd] border-b overflow-hidden transition-all duration-300 h-dvh pointer-events-auto grid grid-rows-3 grid-cols-1 sm:grid-rows-4 sm:grid-cols-2 gap-8">
+    <div className="z-100 fixed left-0 w-screen bg-[#faf0e7] md:bg-[#faf0e7dd] border-b overflow-hidden transition-all duration-300 h-dvh pointer-events-auto grid grid-rows-3 grid-cols-1 md:grid-rows-4 md:grid-cols-2 gap-8">
         <div
           className={cn(
-            "w-full px-2 gap-2 text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-700 select-none flex justify-center items-center text-center sm:col-span-2",
+            "w-full px-2 gap-2 text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-700 select-none flex justify-center items-center text-center md:col-span-2",
             lobster_two.className,
           )}
         >
@@ -30,18 +30,18 @@ export default function Home() {
           </div>
         </div>
         {/* Logo grande izquierda */}
-        <div className="flex items-center justify-center h-full sm:row-span-3">
+        <div className="flex items-center justify-center h-full md:row-span-3 md:p-30">
           <Image
             width={300}
             height={300}
             src="/assets/doritashop_logo.png"
             alt="Dorita Shop"
-            className="w-full h-auto max-w-md object-contain rounded-xl border-5 border-stone-200  select-none"
+            className="w-full h-full object-contain rounded-xl md:border-5 md:border-stone-200  select-none"
           />
         </div>
 
         {/* Cajitas verticales derecha */}
-        <div className="p-4 w-full grid grid-cols-2 sm:flex sm:flex-col sm:px-[10v] gap-4 items-center justify-center h-full sm:row-span-3">
+        <div className="p-4 w-full grid grid-cols-2 md:flex md:flex-col md:px-[10v] gap-4 items-center justify-center h-full md:row-span-3">
           {pages_anchors.map((item) => {
             return (
               <Anchor_card
